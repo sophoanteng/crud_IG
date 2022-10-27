44fd76682a0b678c0f3e5bb9a03d78e6d787fe12
sokphorn <sokphorn080.xuanfang@gmail.com>
2022-10-27 10:48:52 +0700

    Set up project with element ui

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="container">
+    <div slot="header">
+        <b style="font-size: 27px;">Crud IG</b>
+      </div>
+   </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+  }
+</script>
+<style>
+ .container{
+  text-align: center;
+  margin-top: 3%;
+ }
+</style>
